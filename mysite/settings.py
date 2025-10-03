715af35986f5889bf6624b8abf28773d3faf638d
Stachuh <stachu.games.bbc@gmail.com>
2025-10-03 21:24:19 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
+#1
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-a680eonxgw275rzspcv)ykjmr_0lt1p0(j%au!n!7evjw=#lrl'
 
